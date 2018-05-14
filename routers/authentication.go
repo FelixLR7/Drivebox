e111a51cd26f47197512628381cdc2b56f879454
Félix <felix.lorente.7@gmail.com>
2018-05-14 23:24:27 +0200

    Añadido el middleware al index

--- routers/authentication.go
+++ routers/authentication.go
@@ -7,7 +7,7 @@ import (
 
 func SetAuthenticationRoutes(router *mux.Router) *mux.Router {
   router.HandleFunc("/login", controllers.LoginHandler).Methods("POST")
-  router.HandleFunc("/index", controllers.IndexHandler)
+  router.HandleFunc("/index", controllers.CheckAuth(controllers.IndexHandler))
   
   return router
 }
