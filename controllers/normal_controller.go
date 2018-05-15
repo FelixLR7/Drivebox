442fb9c1948143b6e6790d8c82741b5e45548d7c
Félix <felix.lorente.7@gmail.com>
2018-05-15 17:54:32 +0200

    Añadidos handlers para los errores 401 y 404

--- controllers/normal_controller.go
+++ controllers/normal_controller.go
@@ -4,14 +4,16 @@ import (
 	"net/http"
 	"html/template"
 	"log"
-	"fmt"
-	"database/sql"
-	"github.com/go-sql-driver/mysql"
+	_ "fmt"
+	_ "database/sql"
+	"path/filepath"
 )
 
 type Prueba2 struct {
 	User string
 }
+
+var staticFilesPath, _ = filepath.Abs("./src/drivebox/static")
 
 func init() {
 	log.SetPrefix("LOG NORMAL CONTROLLER: ")
@@ -19,14 +21,24 @@ func init() {
 }
 
 func AuthHandler(response http.ResponseWriter, request *http.Request) {
-	tmpl := template.Must(template.ParseFiles(layoutsAbsPath + "/auth.html"))
+	tmpl := template.Must(template.ParseFiles(staticFilesPath + "/auth.html"))
 	prueba := Prueba2{User: "a"}
-	db, err := sql.Open("mysql", "root:123456@/test")
+	/* db, err := sql.Open("mysql", "root:123456@/test")
 
 	if err != nil {
 		fmt.Println("MAL OPEN")
 	}
-	defer db.Close()
+	defer db.Close() */
 
 	tmpl.Execute(response, prueba)
+}
+
+func NotFound404(response http.ResponseWriter, request *http.Request) {
+	tmpl := template.Must(template.ParseFiles(staticFilesPath + "/404.html"))
+	tmpl.Execute(response, nil)
+}
+
+func Unauthorized401(response http.ResponseWriter, request *http.Request) {
+	tmpl := template.Must(template.ParseFiles(staticFilesPath + "/401.html"))
+	tmpl.Execute(response, nil)
 }
