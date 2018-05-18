822f26eda833c87f4888c28f15c0fadb628a6e20
Félix <felix.lorente.7@gmail.com>
2018-05-19 01:25:33 +0200

    Cambiado el manejador para el index

--- routers/normal.go
+++ routers/normal.go
@@ -7,11 +7,5 @@ import (
 
 func SetNormalRoutes() {
 	http.Handle("/static/css/", http.StripPrefix("/static/css", http.FileServer(http.Dir("/home/felix/go/src/drivebox/static/css/"))))
-	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
-		if request.URL.Path != "/" {
-			controllers.ErrorHandler(response, request, http.StatusNotFound)
-		} else {
-			http.ServeFile(response, request, "/home/felix/go/src/drivebox/static/auth.html")
-		}
-	})
+	http.HandleFunc("/", controllers.IndexHandler)
 }
