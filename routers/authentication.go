d59bf2ffff7ec74c82068d59d861c73e89a014d6
Félix <felix.lorente.7@gmail.com>
2018-05-19 18:51:50 +0200

    Añadida ruta para subir archivos

--- routers/authentication.go
+++ routers/authentication.go
@@ -8,6 +8,12 @@ import (
 func SetAuthenticationRoutes() {
 	http.HandleFunc("/login", controllers.LoginHandler)
 	http.HandleFunc("/logout", controllers.LogoutHandler)
-	http.HandleFunc("/upload", controllers.UploadHandler)
+	http.HandleFunc("/upload", func(response http.ResponseWriter, request *http.Request) {
+		if request.Method == "GET" {
+			controllers.UploadPageHandler(response, request)
+		} else {
+			controllers.UploadHandler(response, request)
+		}
+	})
 	http.HandleFunc("/index", controllers.Authentication(controllers.Homepage))
 }
