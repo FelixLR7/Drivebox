19ab47a251b5deaf24edb71bf56ce00f51c34b78
Félix <felix.lorente.7@gmail.com>
2018-05-19 17:26:36 +0200

    Cambiadas las rutas absolutas por relativas

--- controllers/normal_controller.go
+++ controllers/normal_controller.go
@@ -30,22 +30,22 @@ func IndexHandler(response http.ResponseWriter, request *http.Request) {
 			http.Redirect(response, request, "/index", http.StatusFound)
 		} else {
 			fmt.Println(projectPath)
-			http.ServeFile(response, request, "/home/felix/go/src/drivebox/static/auth.html")
+			http.ServeFile(response, request, staticFilesPath+"/auth.html")
 		}
 	}
 }
 
 // ErrorHandler ...
 func ErrorHandler(response http.ResponseWriter, request *http.Request, status int) {
-	http.ServeFile(response, request, "/home/felix/go/src/drivebox/static/errors/"+strconv.Itoa(status)+".html")
+	http.ServeFile(response, request, staticFilesPath+"/errors/"+strconv.Itoa(status)+".html")
 }
 
 // RegisterPageHandler ...
 func RegisterPageHandler(response http.ResponseWriter, request *http.Request) {
-	http.ServeFile(response, request, "/home/felix/go/src/drivebox/static/register.html")
+	http.ServeFile(response, request, staticFilesPath+"/register.html")
 }
 
-// RegisterHandler
+// RegisterHandler ...
 func RegisterHandler(response http.ResponseWriter, request *http.Request) {
 	fmt.Println("registra")
 	http.Redirect(response, request, "/", http.StatusFound)
