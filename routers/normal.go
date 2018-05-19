9b3494f1161370b0c9429d2b2a1b37db38b2164f
Félix <felix.lorente.7@gmail.com>
2018-05-19 17:20:35 +0200

    Creadas las rutas para registrarse, tanto la vista como el método 'RegisterHandler

--- routers/normal.go
+++ routers/normal.go
@@ -7,5 +7,13 @@ import (
 
 func SetNormalRoutes() {
 	http.Handle("/static/css/", http.StripPrefix("/static/css", http.FileServer(http.Dir("/home/felix/go/src/drivebox/static/css/"))))
+	/* http.HandleFunc("/register", controllers.RegisterPageHandler) */
+	http.HandleFunc("/register", func(response http.ResponseWriter, request *http.Request) {
+		if request.Method == "GET" {
+			controllers.RegisterPageHandler(response, request)
+		} else {
+			controllers.RegisterHandler(response, request)
+		}
+	})
 	http.HandleFunc("/", controllers.IndexHandler)
 }
