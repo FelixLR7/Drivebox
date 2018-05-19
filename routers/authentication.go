58679de2786bd9a33bf5ceae5fcad5212bcf0145
Félix <felix.lorente.7@gmail.com>
2018-05-19 10:52:31 +0200

    Añadida ruta para el logout

--- routers/authentication.go
+++ routers/authentication.go
@@ -7,5 +7,6 @@ import (
 
 func SetAuthenticationRoutes() {
 	http.HandleFunc("/login", controllers.LoginHandler)
+	http.HandleFunc("/logout", controllers.LogoutHandler)
 	http.HandleFunc("/index", controllers.Authentication(controllers.Homepage))
 }
